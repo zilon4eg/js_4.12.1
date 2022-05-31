41888c20b9fc65e8f2bc67fe0da5406e045e6700
zilon4eg <hdd500t@gmail.com>
2022-05-31 15:20:43 +0300

    add clear textArea

--- task.js
+++ task.js
@@ -8,3 +8,10 @@ textArea.addEventListener('keyup', () => {
     const textContent = textArea.value;
     localStorage.setItem('text', textContent);
 });
+
+const btn = document.querySelector('.card-clear');
+
+btn.addEventListener('click', () => {
+    textArea.value = '';
+    localStorage.removeItem('text');
+});
