013dee867a3991ea7198815ac4ac4ad93e088e31
zilon4eg <hdd500t@gmail.com>
2022-06-01 16:14:57 +0300

    work on bugs

--- task.js
+++ task.js
@@ -1,8 +1,6 @@
 const textArea = document.getElementById('editor');
 
-if (localStorage.getItem('text')) {
-    textArea.value = localStorage.getItem('text');
-}
+textArea.value = localStorage.getItem('text');
 
 textArea.addEventListener('keyup', () => {
     const textContent = textArea.value;
